a1f328f4b9f8b96157dae4cb7511d221db424957
Christian Bäumlisberger <C.Baeumlisberger@gmail.com>
2025-01-27 20:37:12 +0100

    DevExpress-Sample

---
+++ WpfDx22_4EditorAiDemo/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.AIIntegration;
+using Microsoft.Extensions.AI;
+using System;
+using System.Globalization;
+using System.Threading;
+
+namespace WpfDxAI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : System.Windows.Application
+{
+    public App()
+    {
+        CultureInfo culture = CultureInfo.CreateSpecificCulture("de-DE");
+        Thread.CurrentThread.CurrentUICulture = culture;
+        Thread.CurrentThread.CurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentCulture = culture;
+        CultureInfo.DefaultThreadCurrentUICulture = culture;
+        IChatClient asChatClient = new OllamaChatClient(new Uri("http://localhost:11434/"), "llama3.1:8b");
+        AIExtensionsContainerDesktop.Default.RegisterChatClient(asChatClient);
+    }
+
+}
